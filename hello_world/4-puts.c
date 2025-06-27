bcba97b322cade6a1b40e32683e5502faf4b82c9
root <bundhoor50@gmail.com>
2025-06-27 06:50:43 +0000

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
-gcc
 
-#include <studio.h>
+
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: always 0
+ * Return: always  0
  */
 int main(void)
 {
-
+	puts("/"programing is like bulding a multilingual puzzle");
 }
 
