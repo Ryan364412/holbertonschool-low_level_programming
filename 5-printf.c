f3e4b6973c2fc5483c42540a637baee3406a9d63
Ryan364412 <bundhoor50@gmail.com>
2025-06-28 06:50:29 +0000

    update

---
+++ 5-printf.c
@@ -0,0 +1,13 @@
+
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+       printf "with proper grammar, but the outcome is a piece of art,"
+       return 0 ;
+}
