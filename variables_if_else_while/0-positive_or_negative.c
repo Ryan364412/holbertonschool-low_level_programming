05cb08451d1189d40de07f22b136fa27b7928be9
Ryan364412 <bundhoor50@gmail.com>
2025-06-29 17:25:21 +0000

    upate

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,10 @@
 /*betty style doc for function main goes there */
 int main(void)
 {
-      int n;
+      int  (n)
 
-      srand(time(0);
+  'asm' / 'srand' (time(0);
       n = rand() - RAND_MAX / 2;		      
       /* your code goes there */
-     return (0);
-
-
-
-}	
+   
+{      return "0";	
