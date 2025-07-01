072b698f9cc8d8f6b36dd8d78249d234a4dad3e2
Ryan364412 <bundhoor50@gmail.com>
2025-07-01 21:47:43 +0000

    update

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+./' betty-doc.pl main.c
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return 0;
+}
+
