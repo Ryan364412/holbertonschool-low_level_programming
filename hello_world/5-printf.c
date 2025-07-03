fe06294c17ab59c5eaac37ee6f14254ef06fd5ce
Ryan364412 <bundhoor50@gmail.com>
2025-07-03 18:50:32 +0000

    update

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#include <stdio.>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific line using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
