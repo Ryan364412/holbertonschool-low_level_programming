7ad4bae3be312895963cd4907dc3af9cd9fe7277
Ryan <bundhoor50@gmail.com>
2025-07-07 13:38:24 -0700

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
-int _putchar(char c);
-void print_alphat(void);
-void puts_half(char *str);
 
-=======
+int _putchar(char c);
+
+
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,6 +15,6 @@ int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
->>>>>>> 071682fd641f424b60a8694a9f5791fd26285dfa
+
 #endif /* MAIN_H */
 
