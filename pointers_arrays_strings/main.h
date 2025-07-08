04dd8530ee0160672146126a8561cc5826602765
Ryan364412 <bundhoor50@gmail.com>
2025-07-08 11:58:51 -0700

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,12 @@ void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+
+
+
+
+
 #endif /* MAIN_H */
 
