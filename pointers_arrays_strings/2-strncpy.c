54b089c6f0e5ee91549ca293387c1add07d1dcf3
Ryan364412 <bundhoor50@gmail.com>
2025-07-08 13:37:24 -0700

    update

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,23 @@
-#include "main.h"  // ensure prototype is declared here
-#include <stddef.h>
+#include "main.h"
 
-char *_strncpy(char *dest, const char *src, int n)
+/**
+ * _strncpy - Copy a string starting from index 0 of `dest`.
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
+ * Return: `dest` edited string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
-    /* Copy characters from src to dest until we hit `n` or end of src */
-    while (i < n && src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    /* If src ended before reaching `n`, pad dest with '\0's */
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
-    return dest;
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; n > i; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
 
