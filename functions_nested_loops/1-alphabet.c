b4958b94a34d078d7e932fd37465fc261a06f52c
Ryan364412 <bundhoor50@gmail.com>
2025-07-13 10:59:44 -0700

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ * print_alphabet - writes the alphabet in lowercase
+ * void: No input parameter
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet(void)
 {
-    char letters[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i = 0;
+	int i;
 
-    while (letters[i] != '\0')
-    {
-        _putchar(letters[i]);
-        i++;
-    }
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
 
 
