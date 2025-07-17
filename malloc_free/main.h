3133997b82e3da42e7debb1435d686f80f5d817a
Ryan364412 <bundhoor50@gmail.com>
2025-07-17 11:51:00 -0700

    update

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -13,7 +13,7 @@ void puts_half(char *strn);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-
+void free_grid(int **grid, int height);
 
 
 
