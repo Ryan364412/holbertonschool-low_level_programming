76acd500eacd85ad01575254967b9d5916e0db1c
Ryan364412 <bundhoor50@gmail.com>
2025-07-19 10:13:45 -0700

    update

---
+++ more_malloc_free/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *strn);
+void *malloc_checked(unsigned int b);
+
+
+
+
+
+
+#endif /* MAIN_H */
+
