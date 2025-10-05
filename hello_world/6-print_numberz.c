5dcd333ee33a81ea0867a77ae36367b647014812
Ryan <bundhoor50@gmail.com>
2025-10-05 14:07:58 +0400

    Update 6-print_numberz.c

--- hello_world/6-print_numberz.c
+++ hello_world/6-print_numberz.c
@@ -1,19 +1,1 @@
-#include<stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints all single digit numbers of base 10 starting from 0,
- * followed by a new line. Uses only putchar twice, no char variables.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-		putchar  (i + 48);
-	putchar(10);
-	return (0);
-}
